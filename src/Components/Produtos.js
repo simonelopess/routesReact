f74b8f1d2eecca8515087e39bdd73b9188b655cb
Simone Lopes da Silva <SIMLSILVA@firjan.com.br>
2020-12-07 19:30:11 -0300

    Adicionada a lista de imagens

--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -2,10 +2,23 @@ import React from 'react'
 import styles from './Produtos.module.css'
 
 const Produtos = () => {
+
+  const [produtos, setProdutos] = React.useState(null);
+
+  React.useEffect(()=>{
+    fetch('https://ranekapi.origamid.dev/json/api/produto').then(r=> r.json()).then(json=>setProdutos(json))
+  }, [])
+
+  if(produtos === null) return null;
   return (
-    <div className={styles.produtos}>
-      <h1>Produtos</h1>
-    </div>
+    <section className={styles.produtos + " animeLeft"}>
+      {produtos.map((produto=>
+        <div key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo}></img>
+          <h1>{produto.nome}</h1>  
+        </div>
+      ))}
+    </section>
   )
 }
 
