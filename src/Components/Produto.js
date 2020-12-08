9767fc7592360e4dbf065f44380e72aad0c9ac4b
Simone Lopes da Silva <SIMLSILVA@firjan.com.br>
2020-12-07 21:14:58 -0300

    Personalização da página produto

--- src/Components/Produto.js
+++ src/Components/Produto.js
@@ -34,13 +34,12 @@ const Produto = () =>{
   return(
     <section className={styles.produto + " animeLeft"}>
       <Head title={`Ranek | ${produto.nome}`} description={`Ranek | Esse é um produto ${produto.nome}`}/>
-      {produto.fotos.map(foto => <img key={foto.src} src={foto.src} alt={foto.titulo}/>)}
+      <div>{produto.fotos.map(foto => <img key={foto.src} src={foto.src} alt={foto.titulo}/>)}</div>
       <div>
         <h1>{produto.nome}</h1>
         <span className={styles.preco}>R${produto.preco}</span>
         <p className={styles.descricao}>{produto.descricao}</p>
       </div>
-      <h1>Produto</h1>
     </section>
   )
 }
